3781fd89148fe0b23e5a43d26a70f89b25907ae1
Mahmood Ahmad <mahmood073@gmail.com>
2026-07-03 18:35:48 +0300

    Background Jobs (#5)
    * init geniric job for syncing the news
    
    * init sync articles command
    
    * run the syncing logic in background every 15m

---
+++ app/Console/Commands/SyncNewsCommand.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Attributes\Description;
+use Illuminate\Console\Attributes\Signature;
+use Illuminate\Console\Command;
+use App\Enums\NewsProvider;
+use App\Jobs\SyncNewsProvider;
+
+#[Signature('app:sync-news-command')]
+#[Description('Sync News Command')]
+class SyncNewsCommand extends Command
+{
+    /**
+     * Execute the console command.
+     */
+    public function handle(): void
+    {
+        foreach (NewsProvider::cases() as $provider) {
+            SyncNewsProvider::dispatch($provider);
+        }
+
+        $this->info('News synchronization jobs dispatched.');
+    }
+}
