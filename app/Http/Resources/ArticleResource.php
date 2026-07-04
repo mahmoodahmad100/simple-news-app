5de8a31a66846aca0f3119393d83debdd0d06ad2
Mahmood Ahmad <mahmood073@gmail.com>
2026-07-04 22:44:12 +0300

    adding more attrs to the article resource response

--- app/Http/Resources/ArticleResource.php
+++ app/Http/Resources/ArticleResource.php
@@ -17,10 +17,30 @@ class ArticleResource extends JsonResource
         return [
             'id' => $this->id,
             'title' => $this->title,
-            'content' => $this->content,
             'slug' => $this->slug,
+            'description' => $this->description,
+            'content' => $this->content,
             'url' => $this->url,
+            'image_url' => $this->image_url,
             'published_at' => $this->published_at,
+            'source' => $this->whenLoaded('source', function () {
+                return [
+                    'id' => $this->source->id,
+                    'name' => $this->source->name,
+                ];
+            }),
+            'author' => $this->whenLoaded('author', function () {
+                return [
+                    'id' => $this->author->id,
+                    'name' => $this->author->name,
+                ];
+            }),
+            'categories' => $this->whenLoaded('categories', function () {
+                return $this->categories->map(fn ($category) => [
+                    'id' => $category->id,
+                    'name' => $category->name,
+                ]);
+            })
         ];
     }
 }
